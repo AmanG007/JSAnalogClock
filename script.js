ab9b3432c26320bea430f04b75a7e95aef833749
amang007 <amangupta.ag36@gmail.com>
2021-01-29 21:17:36 +0530

    interval set

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hourSelector = 20;
-let minSelector = 134;
-let secSelector = 43;
+var date = new Date();
 
-HOURHAND.style.transform = "rotate(" + hourSelector + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minSelector + "deg)";
-SECONDHAND.style.transform = "rotate(" + secSelector + "deg)";
+    console.log(date);
+    
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    
+    console.log("Hour: " + hour + " Minutes: " + min +" Second: " + sec);
+    
+    let hourSelector = (hour*360/12)+(min*(360/60)/12);
+    let minSelector = (min*360/60)+(sec*(360/60)/60);
+    let secSelector = sec*360/60;
+    
+function runTheClock() {
+    
+    hourSelector = hourSelector+(3/360);
+    minSelector = minSelector+(6/60);
+    secSelector = secSelector+6;
+    
+    HOURHAND.style.transform = "rotate(" + hourSelector + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minSelector + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secSelector + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
